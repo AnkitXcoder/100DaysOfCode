919d33c57923624abae89170a7cc110a35b8264e
AnkitX <ankitone02@gmail.com>
2025-03-06 22:27:39 +0530

    Added myapp folder in Java basic

---
+++ Java basic/mymap/MyMap.java
@@ -0,0 +1,13 @@
+package mymap;
+import java.util.*;
+public class MyMap {
+    public static void main(String []args)
+    {
+        //created object of all map class to store data with key and value 
+        HashMap hs=new HashMap();
+        Hashtable ht=new Hashtable();
+        TreeMap tm=new TreeMap();
+        LinkedHashMap lm=new LinkedHashMap();
+
+    }
+}
